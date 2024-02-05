e1ab8210c3635d0601da36bed17e37ae00500be6
SimWPEric <e0968924@u.nus.edu>
2024-02-05 23:14:44 +0800

    Add echo command

--- bot.py
+++ bot.py
@@ -1,6 +1,6 @@
 import logging
 from telegram import Update
-from telegram.ext import ApplicationBuilder, ContextTypes, CommandHandler
+from telegram.ext import filters, MessageHandler, ApplicationBuilder, ContextTypes, CommandHandler
 from config import telegram_token
 
 logging.basicConfig(
@@ -11,10 +11,16 @@ logging.basicConfig(
 async def start(update: Update, context: ContextTypes.DEFAULT_TYPE):
     await context.bot.send_message(chat_id=update.effective_chat.id, text="Hello world")
 
+async def echo(update: Update, context: ContextTypes.DEFAULT_TYPE):
+    await context.bot.send_message(chat_id=update.effective_chat.id, text=update.message.text)
+
 if __name__ == '__main__':
     application = ApplicationBuilder().token(telegram_token).build()
     
     start_handler = CommandHandler('start', start)
     application.add_handler(start_handler)
+
+    echo_handler = MessageHandler(filters.TEXT & (~filters.COMMAND), echo)
+    application.add_handler(echo_handler)
     
     application.run_polling()
