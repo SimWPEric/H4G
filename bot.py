3aa5839f2992ece0438dc18787fd649e407f01f3
SimWPEric <e0968924@u.nus.edu>
2024-02-05 23:18:11 +0800

    Refactor commands

--- bot.py
+++ bot.py
@@ -1,26 +1,21 @@
 import logging
 from telegram import Update
-from telegram.ext import filters, MessageHandler, ApplicationBuilder, ContextTypes, CommandHandler
+from telegram.ext import filters, MessageHandler, ApplicationBuilder, CommandHandler
 from config import telegram_token
+from commands import start_command, echo_command
 
 logging.basicConfig(
     format='%(asctime)s - %(name)s - %(levelname)s - %(message)s',
     level=logging.INFO
 )
 
-async def start(update: Update, context: ContextTypes.DEFAULT_TYPE):
-    await context.bot.send_message(chat_id=update.effective_chat.id, text="Hello world")
-
-async def echo(update: Update, context: ContextTypes.DEFAULT_TYPE):
-    await context.bot.send_message(chat_id=update.effective_chat.id, text=update.message.text)
-
 if __name__ == '__main__':
     application = ApplicationBuilder().token(telegram_token).build()
     
-    start_handler = CommandHandler('start', start)
+    start_handler = CommandHandler('start', start_command.start)
     application.add_handler(start_handler)
 
-    echo_handler = MessageHandler(filters.TEXT & (~filters.COMMAND), echo)
+    echo_handler = MessageHandler(filters.TEXT & (~filters.COMMAND), echo_command.echo)
     application.add_handler(echo_handler)
     
     application.run_polling()
